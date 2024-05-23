e01c3328ca252fd94c49d9c055d6cb5e5d14ccd6
Steve-Tobias <sblaket@gmail.com>
2024-05-23 05:00:27 -0600

    modified: main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
     
     print('Welcome to the B33FWare ASCII Text2Art Converter.')
     print('Author: Steven Blake Tobias')
-    print('\n')
+    print()
     input('Press [Enter] or [Return] to continue...')
     print()
     
