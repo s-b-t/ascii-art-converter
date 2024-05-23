48b41204f4b1a43e73a63d4bb9c82c3aaf7b08c6
Steve-Tobias <sblaket@gmail.com>
2024-05-23 04:52:01 -0600

    modified: main.py, added validations

--- main.py
+++ main.py
@@ -12,30 +12,31 @@ def main():
     print('Author: Steven Blake Tobias')
     print('\n')
     input('Press [Enter] or [Return] to continue...')
-    print('\n')
+    print()
     
     while True:
         userText = input('Enter or paste the text you would like to convert to ASCII art (to Quit, type QUIT): ')
-        print('\n')
+        print()
 
         if not (userText):
-            print('You didn\'t enter anything!')
-            print('\n')
+            print('You didn\'t provide any text!')
+            print()
             continue
 
         if userText == 'QUIT':
-            print('Thanks for using the B33FWare ASCII Text2Art Converter!')
+            print('Thanks for using the B33FWare ASCII Text2Art Converter.')
             sys.exit()
         
         if currentFont:
-            useSameFont = input('Do you want to continue with the same font? (Y/N): ')
-            print('\n')
-            # Line 30-34 bugged. Must resolve program only being able to accept Y/N answers for both inputs (31 and 35). Program allows user to just hit Enter or type in a word that generates ASCII (NOT VALID INPUT FOR THESE INPUTS!). Program needs to function just like lines 18-24. Input on Line 35 randomizes no matter yes/no. Yes/no input on line 31 work just fine.
-            if not (useSameFont):
-                input('Please answer (Y/N): ')
-                print('\n')
-            # Line 35-42 there's another bug. When user inputs 'y' the output does not keep the font the same and randomizes instead. Potential problem/placement with while loop. Also need more validations just like note on Line 34.
-            if useSameFont.lower() == 'y':
+            while True:
+                useSameFont = input('Do you want to continue with the same font? (Y/N): ')
+                print()
+                if useSameFont.lower().upper() in ['y', 'n', 'yes', 'no', 'Y', 'N', 'Yes', 'No', 'YES', 'NO' ]:
+                    break
+                else: 
+                    print("You didn't choose an answer! (Y/N): ")
+                    print()
+            if useSameFont.lower().upper() in ['y', 'yes', 'Y', 'Yes', 'YES']:
                 fontToUse = currentFont
             else: 
                 fontToUse = random.choice(fonts)
@@ -44,7 +45,7 @@ def main():
 
         asciiArt = textToAscii(userText, fontToUse)
         print(asciiArt)
-        print('\n')
+        print()
 
         currentFont = fontToUse
 
